50ad51d17986046f154f21f21edeb88a226ed975
Elmar Puts <elmarputs@gmail.com>
2020-11-10 21:38:48 +0100

    Updated API for approximate planet positions ephemeris settings

--- docs/source/_src_snippets/simulation/environment_setup/environment_models/approximate_planet_positions_ephemeris.py
+++ docs/source/_src_snippets/simulation/environment_setup/environment_models/approximate_planet_positions_ephemeris.py
@@ -1,1 +1,4 @@
-body_settings.get_body( "Jupiter" ).ephemeris_settings = environment_setup.ephemeris.approximate_planet_position( )
+body_identifier = environment_setup.ephemeris.BodiesWithEphemerisData.jupiter
+use_circular_coplanar_approximation = False
+
+body_settings.get_body( "Jupiter" ).ephemeris_settings = environment_setup.ephemeris.approximate_planet_positions( body_identifier, use_circular_coplanar_approximation )
