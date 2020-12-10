882653bdc734374251421a20dcfff1293e304cb7
ahjbakx <a.h.j.bakx@student.tudelft.nl>
2020-12-10 15:55:47 +0100

    Small fix RKF78

--- docs/source/_src_snippets/simulation/integrator_setup/runge_kutta_fehlberg.py
+++ docs/source/_src_snippets/simulation/integrator_setup/runge_kutta_fehlberg.py
@@ -1,6 +1,6 @@
 initial_time = 86400 # seconds since J2000
 initial_time_step = 10 # seconds
-coefficient_set = propagation_setup.CoefficientSets.runge_kutta_fehlberg78
+coefficient_set = propagation_setup.CoefficientSets.runge_kutta_fehlberg_78
 minimum_step_size = 0.1 # seconds
 maximum_step_size = 100 # seconds
 relative_error_tolerance = 1.0e-9 # -
