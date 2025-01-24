eaf6f93489f9c9221c6058d07ecac1f0e60b116b
Lars Hinüber <105521415+larshinueber@users.noreply.github.com>
2025-01-24 23:37:06 +0100

    add global imports for numerical simulation modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -220,3 +220,7 @@ intersphinx_disabled_reftypes = []
 
 # -- Options for sphinx_codeautolink -------------------------------------------
 codeautolink_concat_default = True
+codeautolink_global_preface = """
+from tudatpy import numerical_simulation
+from tudatpy.numerical_simulation import propagation_setup, environment_setup, estimation_setup, propagation, environment, estimation
+"""
